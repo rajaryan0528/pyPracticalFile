9bcdb14f662db0922dc0a20ec2ce2a2a3206e6ad
rajaryan0528 <rajaryan0528@gmail.com>
2022-09-01 02:20:30 +0530

    Initial Commit

---
+++ q1.py
@@ -0,0 +1,7 @@
+#Write a function that takes the lengths of three sides:side1,side2 and side3
+#of the triangle as the input from the user using input function and return the area and perimeter of the triangle
+#as a tuple.Also,assert that sum of the length of any two sides is greater than the third side.
+
+def area(a,b,c):
+
+    return area,s*2
